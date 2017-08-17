d1f1cdf81c5ad508baad6b6a6191475635095b09
DYefremov <dmitry.v.yefremov@gmail.com>
2017-08-17 14:47:05 +0300

    ui building

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import gi
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-
 from ui.settings import SettingsDialog
 
 
@@ -10,30 +9,35 @@ def on_status_popup_menu(menu, event_button, event_time):
     menu.popup(None, None, None, menu, event_button, event_time)
 
 
-def show_main_window(menu):
+def show_settings_dialog(menu):
     settings_dialog.show_settings_dialog()
 
 
 def on_exit(menu):
-    # exit_dialog.show()
-    Gtk.main_quit()
+    bl = Gtk.Builder()
+    bl.add_from_file("ui/exit_dialog.glade")
+    exit_dialog = bl.get_object("exit_dialog")
+    resp = exit_dialog.run()
+    if resp == Gtk.ResponseType.OK:
+        print("The OK button was clicked")
+    else:
+        print("The exit was clicked")
+        exit_dialog.destroy()
 
 
 handlers = {
     "on_exit": Gtk.main_quit,
     "status_popup_menu": on_status_popup_menu,
     "on_exit_item": on_exit,
-    "on_show_item": show_main_window
+    "on_show_item": show_settings_dialog
 }
 
 builder = Gtk.Builder()
 builder.add_from_file("ui/status.glade")
-builder.add_from_file("ui/exit_dialog.glade")
-builder.connect_signals(handlers)
-
 settings_dialog = SettingsDialog()
+builder.connect_signals(handlers)
 tryIcon = builder.get_object("status")
-exit_dialog = builder.get_object("exit_dialog")
+builder.connect_signals(handlers)
 
 if __name__ == "__main__":
     Gtk.main()
--- ui/settings.py
+++ ui/settings.py
@@ -1,7 +1,5 @@
 import pwd
-import gi
 
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
@@ -13,20 +11,40 @@ def get_users_list():
     return users_list
 
 
-class SettingsDialog:
+def on_about_dialog(item):
+    builder = Gtk.Builder()
+    builder.add_from_file("ui/about_dialog.glade")
+    dialog = builder.get_object("about_dialog")
+    dialog.run()
+    dialog.hide()
 
+
+class SettingsDialog:
     _builder = Gtk.Builder()
     _builder.add_from_file("ui/main.glade")
     _mainWindow = _builder.get_object("main_window")
+    # init ui elements
+    _cancel_button = _builder.get_object("cancel_button")
+    _apply_button = _builder.get_object("apply_button")
+    _exit_menu_item = _builder.get_object("exit_menu_item")
+    _about_menu_item = _builder.get_object("about_menu_item")
+    _session_duration = _builder.get_object("session_duration")
+    _pause_between_sessions = _builder.get_object("pause_between_sessions")
+
+    handlers = {
+        "on_about_menu_item_activate": on_about_dialog
+    }
+
+    _builder.connect_signals(handlers)
 
     def __init__(self):
         self._init_users_box()
 
     def show_settings_dialog(self):
-        self._mainWindow.show_all()
+        self._mainWindow.show()
 
     def hide_settings_dialog(self):
-        self._mainWindow.destroy()
+        self._mainWindow.hide()
 
     def _init_users_box(self):
         users = get_users_list()
@@ -41,3 +59,7 @@ class SettingsDialog:
         cell = Gtk.CellRendererText()
         self.users_box.pack_start(cell, True)
         self.users_box.add_attribute(cell, "text", 0)
+
+
+if __name__ == "__main__":
+    pass
