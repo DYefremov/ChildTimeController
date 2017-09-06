64fc867a02222cd39126033b6182b9de98379c96
DYefremov <dmitry.v.yefremov@gmail.com>
2017-09-06 15:54:16 +0300

    changed exit with gksu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gi
+import subprocess
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -14,23 +15,15 @@ def show_settings_dialog(menu):
 
 
 def on_exit(menu):
-    bl = Gtk.Builder()
-    bl.add_from_file("ui/exit_dialog.glade")
-    exit_dialog = bl.get_object("exit_dialog")
-    resp = exit_dialog.run()
-    if resp == Gtk.ResponseType.OK:
-        login = bl.get_object("login_field")
-        password = bl.get_object("password_field")
-        print("login = " + login.get_text())
-        print("password = " + password.get_text())
-        exit_dialog.destroy()
-    else:
-        print("The exit was clicked")
-        exit_dialog.destroy()
+    out = subprocess.getoutput("gksu -m 'ChildController' 'ls'")
+    # Current file name
+    res = __file__.split("/")[-1:][0]
+    if res in out:
+        print("PASS")
+        Gtk.main_quit()
 
 
 handlers = {
-    "on_exit": Gtk.main_quit,
     "status_popup_menu": on_status_popup_menu,
     "on_exit_item": on_exit,
     "on_show_item": show_settings_dialog
