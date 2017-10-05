a05fa115f35609a842d0e62f9c0624b55fab97b2
DYefremov <dmitry.v.yefremov@gmail.com>
2017-10-05 23:40:14 +0300

    little logger changes

--- logger.py
+++ logger.py
@@ -1,15 +1,14 @@
 import logging
-import datetime
-import pwd
-import os
+from subprocess import getoutput
 
-LOG_FILE = "log.log"
+LOG_FILE = "/home/dimon/log.log"
 DATE_FORMAT = "%d-%m-%y %H:%M:%S %w"  # 'w' is current day (0-6)
 
 logging.Logger("main_logger")
 logging.basicConfig(level=logging.INFO,
                     filename=LOG_FILE,
-                    format="%(asctime)s %(message)s",
+                    # format="%(asctime)s %(message)s",
+                    format="%(message)s",
                     datefmt=DATE_FORMAT)
 
 
@@ -21,16 +20,39 @@ def is_in_file(f, text):
     return any(text in line for line in f)
 
 
+def is_user_in_list(*args):
+    """"
+       Checking the need for logging for this user(s).
+
+        Need implementation.!!!
+       """
+    return True
+
+
 def open_log():
     with open(LOG_FILE) as file:
-        time = datetime.datetime.now()
-        day = time.isoweekday()
-        date = time.date().strftime(DATE_FORMAT)
-        if is_in_file(file, str(date)):
-            print(date)
+        lines = file.readlines()
+        for line in lines:
+            print(line)
+
+
+def log_login():
+    """
+    Used to redirect logs from 'last'.
+
+    It is necessary because wtmp can be in tmpfs (as at me).
+    """
+    logger = get_logger()
+    with open(LOG_FILE) as file:
+        lines = set(file.readlines())
+    # logger.info(pwd.getpwuid(os.geteuid()).pw_name)
+    out = getoutput("last -R").splitlines()
+    sep = str("(")
+    end = "\n"
+    for line in out:
+        if line + end not in lines and sep in line:
+            logger.info(line)
 
 
 if __name__ == "__main__":
-    logger = get_logger()
-    logger.info(pwd.getpwuid(os.geteuid()).pw_name)
-    open_log()
+    log_login()
